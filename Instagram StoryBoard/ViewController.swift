d183c7163ffacf852187102656001ea1fbab5545
SShrey1 <palishrey@gmail.com>
2024-11-16 01:47:17 +0530

    Initial Commit

---
+++ Instagram StoryBoard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Instagram StoryBoard
+//
+//  Created by user@59 on 25/08/1946 Saka.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Instagram StoryBoardTests/Instagram_StoryBoardTests.swift
@@ -0,0 +1,17 @@
+//
+//  Instagram_StoryBoardTests.swift
+//  Instagram StoryBoardTests
+//
+//  Created by user@59 on 25/08/1946 Saka.
+//
+
+import Testing
+@testable import Instagram_StoryBoard
+
+struct Instagram_StoryBoardTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
